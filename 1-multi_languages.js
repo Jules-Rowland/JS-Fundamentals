a918b668067b46f74d2ebf03752467b07be8e18d
Jules.Rowland <Jules.rowland02@gmail.com>
2025-06-27 21:00:48 +0100

    Multilines

---
+++ 1-multi_languages.js
@@ -0,0 +1,14 @@
+/**Write a script that prints 3 lines:
+
+The first line: “C is fun”
+The second line: “Python is cool”
+The third line: “JavaScript is amazing”
+You must use console.log(...) to print all output
+You are not allowed to use var**/
+
+let Cline = 'C is fun';
+let Pline = 'Python is cool';
+let Jline = 'JavaScript is amazing';
+console.log(Cline);
+console.log(Pline);
+console.log(Jline);
