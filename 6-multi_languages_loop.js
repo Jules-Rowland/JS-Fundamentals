895bbc46329bad56104f2e1eb7683e7f23c9ca43
Jules Rowland <jules.rowland02@gmail.com>
2025-07-07 15:24:27 +0100

    Update 6-multi_languages_loop.js new modification

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -2,7 +2,7 @@ const arText = ["C is fun", "Python is cool", "JavaScript is amazing"];
 
 function multi_languages(){
     let i = 0;
-while(i < arText.length)
+while (i < arText.length)
     {
  console.log(arText[i]);
  i++;
